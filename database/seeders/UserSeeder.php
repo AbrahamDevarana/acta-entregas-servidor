61f021195ae385ba60905a703449a795244cb4fc
AbrahamAG <abrahamalvarado@devarana.mx>
2022-03-02 17:42:38 -0600

    Inicializar Repositorio

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Abraham',
+            'email' => 'abrahamalvarado@devarana.mx',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('abraham'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        User::create([
+            'name' => 'Test',
+            'email' => 'test@devarana.mx',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('test'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
